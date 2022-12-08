34fa71ec4645e102238c737c7b1bb8550e688a8e
ncoderlk <ncoderlk@gmail.com>
2022-12-08 22:33:35 +0530

    updated

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  const handleClick = (name, e) => {
+    console.log("hello" + name, e.target);
+  };
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={(e) => handleClick(" alex", e)}>Click me</button>
+    </div>
+  );
+};
+export default Home;
